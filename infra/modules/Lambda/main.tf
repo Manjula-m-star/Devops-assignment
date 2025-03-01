9c593ddaca2ac0d1fbaa6a19d9332dddfa0963a8
Manjula-m-star <manjulas7575@gmail.com>
2025-03-01 22:32:41 +0530

    Delete infra/modules/Lambda directory

--- infra/modules/Lambda/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "node_ecr" {
-  name = "nodejs-api-repo-${var.environment}"
-}
-
-output "ecr_repository_url" {
-  value = aws_ecr_repository.node_ecr.repository_url
-}
