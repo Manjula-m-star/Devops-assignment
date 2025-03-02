39caa16e80722f04a1bdfff4d91d3e2a1f8976c3
Manjula-m-star <manjulas7575@gmail.com>
2025-03-02 22:18:20 +0530

    Update main.tf

--- infra/modules/LAMDA/main.tf
+++ infra/modules/LAMDA/main.tf
@@ -25,6 +25,11 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = var.lambda_function_name
   role          = var.lambda_role_arn 
   package_type  = "Image"
+  image_uri = (
+  length(data.aws_ecr_image.latest_image) > 0 
+  ? "${data.aws_ecr_image.latest_image[0].registry_id}.dkr.ecr.us-west-2.amazonaws.com/${data.aws_ecr_image.latest_image[0].repository_name}:latest" 
+  : "public.ecr.aws/lambda/nodejs:latest"
+)
 
   environment {
     variables = {
