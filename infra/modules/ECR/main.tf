2b36150ec40f883546985a898c8e645627b83e57
Manjula-m-star <manjulas7575@gmail.com>
2025-03-02 09:36:14 +0530

    Update main.tf

--- infra/modules/ECR/main.tf
+++ infra/modules/ECR/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"  # Set an appropriate AWS provider version
-    }
-  }
-  required_version = ">= 1.0.0"  # Set your Terraform version requirement
-}
-
 resource "aws_ecr_repository" "app_repo" {
   name = var.repo_name  # Uses the variable
 }
