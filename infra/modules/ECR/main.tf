55a9a604cf8f9bb872b2e8ad96f8d668be3f07eb
manjula-m-star <manjula7575@gmail.com>
2025-03-03 11:28:10 +0530

    change in iam and ecr main.tf

--- infra/modules/ECR/main.tf
+++ infra/modules/ECR/main.tf
@@ -1,15 +1,16 @@
 # Check if ECR repository already exists
 data "aws_ecr_repository" "existing_repo" {
   name = "my-demo-repo"
-}
-
-# Create ECR repository only if it doesn't exist
-#resource "aws_ecr_repository" "app_repo" {
-  #name = "my-demo-repo"
   
+
   lifecycle {
     ignore_changes = [image_tag_mutability, encryption_configuration]  # Prevent Terraform from modifying existing settings
   }
 
   depends_on = [data.aws_ecr_repository.existing_repo]
 }
+
+# Create ECR repository only if it doesn't exist
+#resource "aws_ecr_repository" "app_repo" {
+  #name = "my-demo-repo"
+#}
